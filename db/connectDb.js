a0f5f29f7e59c01c51dba51b00e88abcc30976f0
Jeff Lapp <lappjeff20@gmail.com>
2020-01-22 20:05:47 -0500

    Created connectDB module for mongo connection

---
+++ db/connectDb.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const config = require("./mongoConfig");
+const mongoose = require("mongoose");
+
+const connectDb = async () => {
+	try {
+		await mongoose.connect(process.env.MONGO_DB_URI, config);
+		console.log("MongoDB successfully started");
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+module.exports = connectDb;
