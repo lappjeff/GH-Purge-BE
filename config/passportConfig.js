c92bccf2597b0cfde1f3dc12f8a82f07d87cfba8
Jeff Lapp <lappjeff20@gmail.com>
2020-01-23 13:41:03 -0500

    Added comments for passport flow

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -6,7 +6,9 @@ const clientID = process.env.AUTH_CLIENT_ID;
 const clientSecret = process.env.AUTH_SECRET;
 const callbackURL = process.env.AUTH_CALLBACK;
 
+// called after CB used in strategy setup below
 passport.serializeUser((user, done) => {
+	// passes data to deserializeUser through 2'nd done param
 	done(null, user);
 });
 
@@ -22,6 +24,7 @@ passport.use(
 			callbackURL
 		},
 		function(accessToken, refreshToken, profile, done) {
+			// done in this case passes given data to serializeUser
 			return done(null, { accessToken, profile });
 		}
 	)
